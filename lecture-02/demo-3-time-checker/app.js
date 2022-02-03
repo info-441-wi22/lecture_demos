a5ddde2cba710141055e277bb07df6be8dc3ec0c
Jaimie Jin <jinjaimie12@gmail.com>
2022-02-03 10:55:06 -0800

    change fs

--- lecture-02/demo-3-time-checker/app.js
+++ lecture-02/demo-3-time-checker/app.js
@@ -1,23 +1,23 @@
-import  * as fs from 'fs';
+import  {readFileSync} from 'fs';
 import dateFormat from 'dateFormat';
 import express from 'express';
 const app = express()
 
 app.get('/', (req, res) => {
   res.type('html')
-  let fileContents = fs.readFileSync("index.html")
+  let fileContents = readFileSync("index.html")
   res.send(fileContents)
 })
 
 app.get('/style.css', (req, res) => {
   res.type('css')
-  let fileContents = fs.readFileSync("style.css")
+  let fileContents = readFileSync("style.css")
   res.send(fileContents)
 })
 
 app.get('/index.js', (req, res) => {
   res.type('js')
-  let fileContents = fs.readFileSync("index.js")
+  let fileContents = readFileSync("index.js")
   res.send(fileContents)
 })
 
