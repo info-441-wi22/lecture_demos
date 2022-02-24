27f097abc6cd295992470fd09761b2d9d9a18794
kyle thayer <kyle.thayer@gmail.com>
2022-02-24 11:41:43 -0800

    finished store example

--- lecture-13/demo-2-store-finish/routes/items.js
+++ lecture-13/demo-2-store-finish/routes/items.js
@@ -15,22 +15,62 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/store');
   console.log("connected to mongodb");
 
-  //TODO: Add schemas and models
+  //Add schemas and models
+  const itemSchema = new mongoose.Schema({
+    name: String,
+    price: Number
+  })
+  Item = mongoose.model('Item', itemSchema)
 }
 
 
-// get json data for all users
+// get json data for all items
 router.get('/', async function(req, res, next) {
-  res.json([]);
+  let allItems = await Item.find()
+  res.json(allItems);
 });
 
 router.post('/saveCart', async function(req, res, next) {
+  // body looks like this:
+  //[{"itemId":"6217d6fdfecdc9ba998584cb","itemCount":"1"},{"itemId":"6217d757fecdc9ba998584cc","itemCount":"2"}]
+  let cartInfo = req.body
+  console.log("saving cart infor into session: ", cartInfo)
+  req.session.cartInfo = JSON.stringify(cartInfo)
+  console.log("session info is now: ", req.session)
+  
   res.json({status: "success"})
 })
 
-
 router.get('/getCart', async function(req, res, next) {
-  res.json([])
+  let cartInfo = req.session.cartInfo
+  if(!cartInfo){
+    res.json([])
+    return
+  }
+
+  let cartInfoJson = JSON.parse(cartInfo)
+
+  // look up all cart items in the database
+  let cartItemsIds = cartInfoJson.map(cartItem => cartItem.itemId)
+  // load just those items from the database
+  let itemsInfo = await Item.find().where('_id').in(cartItemsIds).exec()
+
+  //combine the cart info with the items info
+
+  // make the itemsInfo look-upable by the id
+  let itemsInfoById = {}
+  itemsInfo.forEach(itemInfo => {
+    itemsInfoById[itemInfo._id] = itemInfo
+  })
+
+  // take the cartInfoJson, and to each item, add the name and price
+  let combinedCartInfo = cartInfoJson.map(cartItem => {
+    cartItem["price"] = itemsInfoById[cartItem.itemId].price
+    cartItem["name"] = itemsInfoById[cartItem.itemId].name
+    return cartItem
+  })
+
+  res.json(combinedCartInfo)
 })
 
 
